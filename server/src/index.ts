2089b42787b5b667583fbc7b0b87871b23f9cc59
NinadMaladkar <maladkar.ninad@gmail.com>
2023-02-02 16:23:45 +0100

    Create endpoints for get and post for decks

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,16 @@ import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import DeckModel from './models/Deck';
 import { config } from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
+config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.post('/decks', async (req: Request, res: Response) => {
   const newDeck = new DeckModel({
@@ -16,9 +22,19 @@ app.post('/decks', async (req: Request, res: Response) => {
   res.json(result);
 });
 
+app.get('/decks', async (req: Request, res: Response) => {
+  const allDecks = await DeckModel.find();
+
+  console.log(allDecks);
+
+  res.json(allDecks);
+});
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello There');
 });
+
+console.log(process.env.MONGO_URL);
 
 const PORT = 5000;
 const db = mongoose.connect(process.env.MONGO_URL!).then(() => {
