74c69b7635ad4f8b06d0d730a147b8da2174380c
NinadMaladkar <maladkar.ninad@gmail.com>
2023-02-06 20:23:34 +0100

    Prepare for deployment to heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
-import DeckModel from './models/Deck';
 import { config } from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -30,11 +29,10 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello There');
 });
 
-console.log(process.env.MONGO_URL);
+const port = process.env.PORT || 5000;
 
-const PORT = 5000;
 const db = mongoose.connect(process.env.MONGO_URL!).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}`);
+  app.listen(port, () => {
+    console.log(`Server running on PORT ${port}`);
   });
 });
